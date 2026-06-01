9ccbabe821b19868a4538666b5520bf8d8f3e20e
nastyakrivova <krivovaanastas2@gmail.com>
2026-06-01 10:28:04 +0300

    server data

--- server/src/main/java/com/myorg/lab7/server/ServerMain.java
+++ server/src/main/java/com/myorg/lab7/server/ServerMain.java
@@ -39,10 +39,10 @@ public class ServerMain {
 
         try{
 
-            String dbUrlLocal = "jdbc:postgresql://localhost:5432/testlab7";
-            String dbUserLocal = "postgres";
-            String dbPasswordLocal = "3013";
-            DBManager dbManager = new DBManager(dbUrlLocal, dbUserLocal, dbPasswordLocal);
+            String dbUrl = "jdbc:postgresql://pg/studs";
+            String dbUser = "s502501";
+            String dbPassword = "vxTj72Ecz4qlSlv6";
+            DBManager dbManager = new DBManager(dbUrl, dbUser, dbPassword);
 
 
             CollectionManager collectionManager = new CollectionManager(dbManager);
